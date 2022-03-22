c6223786fb5b5e475959061085a543c6782366a6
viniciu <felipe_mielke-vieira@estudante.sc.senai.br>
2022-03-22 11:12:17 -0300

    22/03

--- materiais-escolares/src/app/usuario/editar-endereco/editar-endereco.component.ts
+++ materiais-escolares/src/app/usuario/editar-endereco/editar-endereco.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EnderecoService } from 'src/app/services/endereco.service';
 
 @Component({
   selector: 'app-editar-endereco',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarEnderecoComponent implements OnInit {
 
-  constructor() { }
+  estado = 0;
+  
+  cidade: String
+  bairro: String
+  rua: String
+
+  complemento = "";
+  numero = null;
+
+  constructor(private enderecoService: EnderecoService, private router: Router) { }
 
   ngOnInit() {
+    this.paisesSelect();
+  }
+
+  paisesSelect() {
+    this.enderecoService.buscarPaises().then(function (result: any) {
+      var self = this;
+      result.forEach(function (e) {
+        let selectPais = document.querySelector('.selectPais')
+        let opcao = document.createElement('option')
+
+        opcao.value = e.CODIGO
+        opcao.innerText = e.NOME
+
+        selectPais.appendChild(opcao);
+      }) 
+    })
+  }
+
+  adicionarEndereco() {
+    this.enderecoService.criarEndereco(this.cidade, this.bairro, this.rua, this.numero, this.complemento, this.estado, localStorage.getItem('codigo'))
+  }
+
+  selectEstado(valor) {
+    if(valor != 0) {
+      this.enderecoService.buscarEstados(valor).then(function (result: any) {
+        console.log(result);
+        result.forEach(function (e) {
+          let selectEstado = document.querySelector('.selectEstado')
+          let opcao = document.createElement('option')
+  
+          opcao.value = e.CODIGO
+          opcao.innerText = e.SIGLA
+          selectEstado.appendChild(opcao);
+        }) 
+      })
+    }
+  }
+
+  estadoSelecionado(valor) {
+    this.estado = valor
   }
 
 }
--- materiais-escolares/src/app/usuario/enderecos/enderecos.component.ts
+++ materiais-escolares/src/app/usuario/enderecos/enderecos.component.ts
@@ -9,15 +9,6 @@ import { EnderecoService } from 'src/app/services/endereco.service';
 })
 export class EnderecosComponent implements OnInit {
 
-  estado = 0;
-  
-  cidade: String
-  bairro: String
-  rua: String
-
-  complemento = "";
-  numero = null;
-
   constructor(private enderecoService: EnderecoService, private router: Router) { }
 
   ngOnInit() {
@@ -26,7 +17,6 @@ export class EnderecosComponent implements OnInit {
     this.enderecoService.buscarEndereco(localStorage.getItem('codigo')).then(function (result) {
       self.listarEnderecos(result);
     })
-    this.paisesSelect();
   }
 
   listarEnderecos(result) {
@@ -38,42 +28,6 @@ export class EnderecosComponent implements OnInit {
 
   adicionarEndereco() {
     this.router.navigate(['/usuario/enderecos/' + 0])
-    // this.enderecoService.criarEndereco(this.cidade, this.bairro, this.rua, this.numero, this.complemento, this.estado, localStorage.getItem('codigo'))
-  }
-
-  paisesSelect() {
-    this.enderecoService.buscarPaises().then(function (result: any) {
-      var self = this;
-      result.forEach(function (e) {
-        let selectPais = document.querySelector('.selectPais')
-        let opcao = document.createElement('option')
-
-        opcao.value = e.CODIGO
-        opcao.innerText = e.NOME
-
-        selectPais.appendChild(opcao);
-      }) 
-    })
-  }
-
-  selectEstado(valor) {
-    if(valor != 0) {
-      this.enderecoService.buscarEstados(valor).then(function (result: any) {
-        console.log(result);
-        result.forEach(function (e) {
-          let selectEstado = document.querySelector('.selectEstado')
-          let opcao = document.createElement('option')
-  
-          opcao.value = e.CODIGO
-          opcao.innerText = e.SIGLA
-          selectEstado.appendChild(opcao);
-        }) 
-      })
-    }
-  }
-
-  estadoSelecionado(valor) {
-    this.estado = valor
   }
 
 }
